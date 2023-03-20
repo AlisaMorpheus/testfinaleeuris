f48f000ca5a307bf8a75fbec48d640577e9e7657
AlisaMorpheus <84151530+AlisaMorpheus@users.noreply.github.com>
2023-03-20 09:19:24 +0100

    initial commit

---
+++ src/main/java/org/example/testfinale/TestfinaleApplication.java
@@ -0,0 +1,13 @@
+package org.example.testfinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/testfinale/TestfinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testfinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
